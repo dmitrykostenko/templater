0f771e89b8c458bd5a6b3e4d8c6f6a3c9ce0da07
Dmitry Kostenko <dkostenko@justcoded.co>
2018-05-18 15:45:54 +0300

    stage 4

--- templater.js
+++ templater.js
@@ -1,27 +1,37 @@
-var Templater = {
-    tags: {},
-
-    addTag: function (tag, template) {
-        this.tags[tag] = template;
-    },
-
-    run: function () {
-        for (let tagName in this.tags) {
-            let customTags = Array.from(document.getElementsByTagName(tagName));
-            customTags.forEach( (item) => {
-                item.outerHTML = this.render(this.tags[tagName], item);
-        });
-        }
-    },
-
-    render: function (template, element) {
-        let result = template.replace(/{{([a-zA-Z]+)}}/g, function (attr, template) {
-            if (template === 'html') {
-                return element.innerHTML;
-            } else {
-                return element.getAttribute(template);
-            }
-        });
-        return result;
+;(function($) {
+    function Templater(opt) {
+        this.options = opt;
+        this.holder = $(opt.holder);
+        this.run();
     }
-};
+    Templater.prototype = {
+        run: function() {
+            var self = this;
+
+            $.each(this.options.tags, function(tagName) {
+                self.holder.find(tagName).each(function() {
+                    var element = $(this);
+                    element.replaceWith(self.render(self.options.tags[tagName], element))
+                });
+            })
+        },
+        render: function (template, element) {
+            let result = template.replace(/{{([a-zA-Z]+)}}/g, function (attr, template) {
+                if (template === 'html') {
+                    //return element.html();
+                    return element.innerHTML = 'Some Text';
+                } else {
+                    return element.attr(template);
+                }
+            });
+            return result;
+        }
+    }
+    $.fn.templater = function(opt) {
+        this.each(function() {
+            new Templater($.extend(opt, {
+                holder: this
+            }))
+        });
+    };
+})(jQuery);
