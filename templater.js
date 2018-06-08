b66326b86fb6b34c3521ba7dc93a6029922bd96e
Dmitry Kostenko <dkostenko@justcoded.co>
2018-06-08 17:45:42 +0300

    stage07

--- templater.js
+++ templater.js
@@ -1,38 +1,39 @@
-function Templater(opt) {
-    this.options = opt;
-    this.holder = opt.holder;
-    this.run();
-}
+class Templater {
+    constructor(opt) {
+        this.holder = opt.holder;
+        this.templates = opt.templates;
 
-Templater.prototype = {
-    run: function() {
-        let self = this;
+        this.run();
+    }
 
-        for (let tag in this.options.tagsTemplate) {
-            let tagsTemplate = this.options.tagsTemplate[tag];
+    run() {
+        for (let tag in this.templates) {
+            const template = this.templates[tag];
+            const elements = Array.from(this.holder.getElementsByTagName(tag));
 
-            function replacePanel(container) {
-                let tagToReplace = container.querySelector(tag);
-                if (tagToReplace) {
-                    let parent = tagToReplace.parentNode;
-                    let tempDiv = document.createElement('div');
-                    tempDiv.innerHTML = self.render(tagsTemplate, tagToReplace);
-                    parent.replaceChild(tempDiv.childNodes[0],tagToReplace);
-                    replacePanel(self.holder);
-                }
-            }
-            replacePanel(this.holder);
+            elements.forEach((element) => {
+                this.replace(element, template)
+            })
         }
-    },
 
-    render: function (template, element) {
-        let result = template.replace(/{{([a-zA-Z]+)}}/g, function (attr, template) {
-            if (template === 'html') {
+        this.isDocumentHasCustomTags();
+    }
+
+    isDocumentHasCustomTags() {
+        console.log(11)
+    }
+
+    replace(element, template) {
+        element.outerHTML = this.render(template, element);
+    }
+
+    render(template, element) {
+        return template.replace(/{{([a-zA-Z]+)}}/g, (template, attr) => {
+            if (attr === 'html') {
                 return element.innerHTML;
             } else {
-                return element.getAttribute(template);
+                return element.getAttribute(attr);
             }
         });
-        return result;
     }
-};
+}
